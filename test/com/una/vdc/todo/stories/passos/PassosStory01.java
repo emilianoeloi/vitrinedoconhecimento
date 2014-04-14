984f1c3b59b3a38c78b48f40f98051bc57c4d4da
Emiliano Silva Barbosa <emilianoeloi@gmail.com>
2014-04-14 19:27:38 -0300

    Classe Aluno BDD

---
+++ test/com/una/vdc/todo/stories/passos/PassosStory01.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.una.vdc.todo.stories.passos;
+
+import com.una.vdc.exception.AssociationException;
+import com.una.vdc.model.course.CollegeClass;
+import com.una.vdc.model.user.MentorTeacher;
+import com.una.vdc.persistence.dao.MentorTeacherDAO;
+import junit.framework.Assert;
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Named;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+/**
+ *
+ * @author Roseane Costa
+ */
+public class PassosStory01 {
+    
+   MentorTeacher mentorTeacher;
+   MentorTeacherDAO  mentorTeacherDAO;
+   CollegeClass collegeClass;
+   AssociationException associationException;
+    
+    @Given("desejo associar <professor-orientador> a turma")    
+    public void getIdMentorTeacher(@Named("<professor-orientador>") Long id){
+         mentorTeacher = new MentorTeacher();
+         mentorTeacher.setId(id);
+     }
+   
+    @When("seleciono a <turma> e clico em salvar")
+     public void getIdCollegeClass(@Named("<turma>") Long id){
+         collegeClass = new CollegeClass();
+         collegeClass.setID(id);
+     }
+    
+    @Then("exibe a seguinte mensagem <mensagem>")  //verificar
+      public void MentorTeacherDAO(String mensagem) throws AssociationException{
+          mentorTeacherDAO.associateMentorTeacherToClass(mentorTeacher, collegeClass);
+          Assert.assertEquals(mensagem, mentorTeacherDAO.toString());
+                  
+                  
+        
+    }
+}
+
+
