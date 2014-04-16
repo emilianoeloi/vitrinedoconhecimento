aed920de6a36108d090ed55a952e7cc607e197af
Emiliano Silva Barbosa <emilianoeloi@gmail.com>
2014-04-15 23:19:27 -0300

    excluindo pastas de build

--- build/web/js/app.js
+++
@@ -1,3 +0,0 @@
-angular.module('vdcApp', ['vdcControllers', 'vdcServices']);
-var vdcControllers = angular.module('vdcControllers', []);
-var vdcServices = angular.module('vdcServices', ['ngResource']);
